823a91447ea6cd206b77a08a1bd146dcfd9fd08f
darknorth <ramandy007@gmail.com>
2019-04-25 22:14:25 +0530

    Bootstrapped

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+import {Form,FormControl,Button,Container, InputGroup}from 'react-bootstrap';
+import "./App.css";
+
+class App extends Component{
+    render(){
+        return(
+            <Container >
+            <div className='App'>
+                <div className="App-title">Music master </div>
+                <div>
+               <Form>
+                    <Form.Group>
+                      <InputGroup > < Form.Control type="email" placeholder ="Search for an  Artist...." />
+                      <InputGroup.Append>
+                      <Button type="submit" variant="outline-secondary">Submit</Button>  
+                      </InputGroup.Append>
+                      </InputGroup> 
+                    </Form.Group>
+
+               </Form>
+                </div>
+                <div className="Profile">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+
+                </div>
+                 <div className="Gallery">Gallery</div>
+
+            </div>
+            </Container>
+
+        );
+    };
+}
+ export default App;
